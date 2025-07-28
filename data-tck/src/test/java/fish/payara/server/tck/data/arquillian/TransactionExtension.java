baea52ada514a721a594e19ae37396bdee45fefe
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2025-07-28 17:09:09 -0600

    FISH-11711: testing more changes to enable injection

--- data-tck/src/test/java/fish/payara/server/tck/data/arquillian/TransactionExtension.java
+++ data-tck/src/test/java/fish/payara/server/tck/data/arquillian/TransactionExtension.java
@@ -1,14 +1,18 @@
 package fish.payara.server.tck.data.arquillian;
 
 import java.lang.reflect.Method;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.InvocationInterceptor;
 import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 
 public class TransactionExtension implements InvocationInterceptor {
 
+    private static final Logger log = Logger.getLogger(TransactionExtension.class.getName());
+    
     @Override
     public void interceptTestMethod(Invocation<Void> invocation, ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext) throws Throwable {
+        log.info("intercepting test method"+invocationContext.toString());
         invocation.proceed();
     }
 }
