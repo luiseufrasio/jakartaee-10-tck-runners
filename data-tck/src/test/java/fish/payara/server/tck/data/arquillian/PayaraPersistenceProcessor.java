617b2eaacb745a45a5fb6985cb8729a32c49d83c
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2025-07-29 22:09:40 -0600

    FISH-11711: fixing issue to execute TCK

--- data-tck/src/test/java/fish/payara/server/tck/data/arquillian/PayaraPersistenceProcessor.java
+++ data-tck/src/test/java/fish/payara/server/tck/data/arquillian/PayaraPersistenceProcessor.java
@@ -22,8 +22,7 @@ public class PayaraPersistenceProcessor implements ApplicationArchiveProcessor {
         if (archive instanceof WebArchive webArchive) {
             webArchive.addAsWebInfResource(getClass().getClassLoader().getResource("persistence.xml"), 
                     "classes/META-INF/persistence.xml")
-                    .addAsWebInfResource(getClass().getClassLoader().getResource("beans.xml"),"classes/META-INF/beans.xml")
-                    .addPackages(false, TransactionExtension.class.getPackage(), EntityTests.class.getPackage())
+                    .addPackages(false, EntityTests.class.getPackage())
                     .addPackages(true, SignatureTest.class.getPackage(), PluginAPI.class.getPackage(),
                             com.sun.tdk.signaturetest.core.Log.class.getPackage(), CommandLineParser.class.getPackage())
                     .add(new ClassLoaderAsset("META-INF/services/org.junit.jupiter.api.extension.Extension"),
